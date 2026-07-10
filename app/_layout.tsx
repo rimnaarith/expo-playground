152f42ed1a93e8e92d021bd647a81d379bad9cbf
rimnaarith <rimnaarith@gmail.com>
2026-07-10 10:35:24 +0700

    chore: set up fonts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,33 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "Roboto-ExtraLight": require("../src/assets/fonts/Roboto-ExtraLight.ttf"),
+    "Roboto-Light": require("../src/assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Regular": require("../src/assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("../src/assets/fonts/Roboto-Medium.ttf"),
+    "Roboto-SemiBold": require("../src/assets/fonts/Roboto-SemiBold.ttf"),
+    "Roboto-Bold": require("../src/assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-ExtraBold": require("../src/assets/fonts/Roboto-ExtraBold.ttf"),
+    "Roboto-Black": require("../src/assets/fonts/Roboto-Black.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+
+  if (!loaded && !error) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar animated style="auto" />
--- src/features/home/screens/HomeScreen.tsx
+++ src/features/home/screens/HomeScreen.tsx
@@ -3,8 +3,23 @@ import { Text, View } from "react-native";
 export function HomeScreen() {
   return (
     <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-2xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-7xl font-roboto-black text-blue-50">Welcome!</Text>
+      <Text className="text-7xl font-roboto-extrabold text-blue-100">
+        Welcome!
+      </Text>
+      <Text className="text-7xl font-roboto-bold text-blue-200">Welcome!</Text>
+      <Text className="text-7xl font-roboto-semibold text-blue-300">
+        Welcome!
+      </Text>
+      <Text className="text-7xl font-roboto-medium text-blue-400">
+        Welcome!
+      </Text>
+      <Text className="text-7xl font-roboto-regular text-blue-500">
+        Welcome!
+      </Text>
+      <Text className="text-7xl font-roboto-light text-blue-600">Welcome!</Text>
+      <Text className="text-7xl font-roboto-extralight text-blue-700">
+        Welcome!
       </Text>
     </View>
   );
