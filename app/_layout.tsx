cce7aa193e4cad20abaa19bef4b4a3c3486124ee
rimnaarith <rimnaarith@gmail.com>
2026-07-10 09:28:02 +0700

    chore: configure StatusBar with auto style and animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }} />;
+  return (
+    <>
+      <StatusBar animated style="auto" />
+      <Stack screenOptions={{ headerShown: false }} />
+    </>
+  );
 }
